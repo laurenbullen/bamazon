9d89df6996df18ce7b70d5f5493f25941cbbee83
Lauren Bullen <laurenbullen@Laurens-MacBook-Pro.local>
2019-03-15 23:19:19 -0700

    additional Readme info

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# bamazon
+# Bamazon
+##Purpose
+To create an Amazon-like marketplace that uses Node.js and MySQL that takes in orders from customers and depletes the stock numbers when a purchase is made.
+
+##Overview
+
+1. Create a database called `bamazon`.
+2. Populate create a table called `products`.
+3. The ``products`` table should have the following columns:
+   
+   * item_id (unique id for each product)
+
+   * product_name (Name of product)
+
+   * department_name
+
+   * price (cost to customer)
+
+   * stock_quantity (how much of the product is available in stores)
+
+4. Add 10 items to populate the `products` table.
+
+![image](https://{Screenshots/database})
+
+5. Then create Node application bamazon.js. Running this application will show a table with all of the 10 products and their associated details (item_id, product_name, department_name, price, stock_quantity).
+
+6. The app will then prompt the user with the following question:
+- `What is the ID of the item you are looking for? [Quit with Q]`
+
+7. The user then either inputs the item_id (integer) or the the letter "Q" to quit. Quiting will end the session with the message `Goodbye!` and responding with an item_id (1-10) will prompt the next question.
+
+8. If an item_id was chosen, the user will be prompted with the following question:
+- `How many would you like to purchase? [Quit with Q]`
+
+9. Once the user enters the quantity  to purchase, the app will check to make sure there is enough quanitity of that product to purchase. 
+- If there is insufficient quantity, the app will log the message `Sorry, we don't have that many in stock!`
+
+10. If there is sufficient quantity, the makePurchase function will run and that number will be deducted from the inventory of that product. This will be reflected in the MySQL database.
+
+
--- bamazon.js
+++ bamazon.js
@@ -3,93 +3,107 @@ var inquirer = require("inquirer");
 require("console.table");
 
 var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "root",
-    database: "bamazon_db"
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "root",
+  database: "bamazon_db"
 });
 
-connection.connect(function(err){
-    console.log("connection successfull");
-    makeTable()
+connection.connect(function(err) {
+  console.log("connection successfull");
+  makeTable();
 });
 
-var makeTable = function(){
-    connection.query("select * from products", function(err, res){
-        console.table(res);
-        selectItem(res);
-    })
-}
-
-var selectItem = function(inventory){
-    inquirer.prompt([{
-        type: "input",
-        name: "item",
-        message: "What is the ID of the item you are looking for? [Quit with Q]",
-        validate: function(value){
-            return !isNaN(value) || value.toLowerCase()==="q"
-        }
-    }]).then(function(value){
-        checkExit(value.item);
-        var choiceId = parseInt(value.item);
-        var product = checkInventory(choiceId, inventory);
-        if (product){
-            selectQuantity(product)
-        } else{
-            console.log("");
-            console.log("That item is not in our inventory.");
-            console.log("");
-        }
-    })
+var makeTable = function() {
+  connection.query("SELECT * FROM products", function(err, res) {
+    console.table(res);
+    selectItem(res);
+  });
 };
 
-var selectQuantity = function(product){
-    inquirer.prompt([{
+var selectItem = function(inventory) {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "item",
+        message:
+          "What is the ID of the item you are looking for? [Quit with Q]",
+        validate: function(value) {
+          return !isNaN(value) || value.toLowerCase() === "q";
+        }
+      }
+    ])
+    .then(function(value) {
+      checkExit(value.item);
+      var choiceId = parseInt(value.item);
+      var product = checkInventory(choiceId, inventory);
+      if (product) {
+        selectQuantity(product);
+      } else {
+        console.log("");
+        console.log("That item is not in our inventory.");
+        console.log("");
+      }
+    });
+};
+
+var selectQuantity = function(product) {
+  inquirer
+    .prompt([
+      {
         type: "input",
         name: "quantity",
         message: "How many would you like to purchase? [Quit with Q]",
-        validate: function(value){
-            return !isNaN(value) || value.toLowerCase()==="q"
+        validate: function(value) {
+          return !isNaN(value) || value.toLowerCase() === "q";
         }
-    }]).then(function(value){
-        checkExit(value.quantity);
-        var quantity = parseInt(value.quantity);
-        if(quantity > product.stock_quantity){
-            console.log("");
-            console.log("Sorry, we don't have that many in stock!");
-            console.log("");
-            makeTable()
-        } else{
-            makePurchase(product, quantity)
-        }
-
-    })
+      }
+    ])
+    .then(function(value) {
+      checkExit(value.quantity);
+      var quantity = parseInt(value.quantity);
+      if (quantity > product.stock_quantity) {
+        console.log("");
+        console.log("Sorry, we don't have that many in stock!");
+        console.log("");
+        makeTable();
+      } else {
+        makePurchase(product, quantity);
+      }
+    });
 };
 
-var checkInventory = function(choiceId, inventory){
-    for (let i = 0; i < inventory.length; i++) {
-        if(inventory[i].item_id === choiceId){
-            return inventory[i]
-        }   
+var checkInventory = function(choiceId, inventory) {
+  for (let i = 0; i < inventory.length; i++) {
+    if (inventory[i].item_id === choiceId) {
+      return inventory[i];
     }
-    return null
+  }
+  return null;
 };
 
-var makePurchase = function(product, quantity){
-    connection.query("UPDATE products set stock_quantity = stock_quantity - ? WHERE item_id = ?", [quantity, product.item_id], function(err, res){
-        console.log("");
-        console.log(`Successfully purchased ${quantity} ${product.product_name}(s)!`)
-        console.log("")
-        makeTable()
-    })
-}
-
-var checkExit = function(choice){
-    if (choice.toLowerCase() === "q"){
-        console.log("");
-        console.log("Goodbye!");
-        console.log("")
-        process.exit(0)
+var makePurchase = function(product, quantity) {
+  connection.query(
+    "UPDATE products SET stock_quantity = stock_quantity - ? WHERE item_id = ?",
+    [quantity, product.item_id],
+    function(err, res) {
+      console.log("");
+      console.log(
+        `Successfully purchased ${quantity} ${product.product_name}(s)!`
+      );
+      console.log("");
+      makeTable();
     }
-}
+  );
+};
+
+var checkExit = function(choice) {
+  if (choice.toLowerCase() === "q") {
+    console.log("");
+    console.log("Goodbye!");
+    console.log("");
+    process.exit(0);
+  }
+};
